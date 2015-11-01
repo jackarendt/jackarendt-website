fcc2d3333a1312d394c00b7f64d535c9d0c2a6d4
jackarendt <jack.arendt93@gmail.com>
2015-10-31 17:20:20 -0700

    removed bootstrap from main page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+(function ($) {
+  $(document).ready(function() {
+    var backgroundURL = '../img/ferrybuilding.jpg'
+    var bgImage = new Image();
+    bgImage.src = backgroundURL;
+    bgImage.onload = function(){
+        var main = $(".main-page");
 
+        var scale = this.height/this.width;
+        var width = $(window).width();
+
+        console.log(width * scale);
+
+        main.css('background-image', 'url(' + backgroundURL + ')');
+        main.css('width', '100%');
+        main.css('height', width * scale + 'px');
+    };
+
+});
+  }(jQuery));
