fcb19182ecf9a246c57c309ff3ad3c815af6acb0
jackarendt <jack.arendt93@gmail.com>
2015-11-01 19:58:32 -0800

    refactored contact to its own js class

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+(function ($) {
+    $(document).ready(function() {
+        $('#twitter').on('click', function(e){
+            window.location.href = 'http://twitter.com/woogidy_woogidy';
+        });
+
+        $('#linkedin').on('click', function(e){
+            window.location.href = 'http://linkedin.com/in/johnrarendt';
+        });
+
+        $('#email').on('click', function(e){
+            window.location.href = 'mailto:jack.arendt1993@gmail.com';
+        });
+    });
+}(jQuery));
--- js/main.js
+++ js/main.js
@@ -33,17 +33,5 @@
             $('.navbar-brand').fadeOut();
         }
     });
-
-    $('#twitter').on('click', function(e){
-        window.location.href = 'http://twitter.com/woogidy_woogidy';
-    });
-
-    $('#linkedin').on('click', function(e){
-        window.location.href = 'http://linkedin.com/in/johnrarendt';
-    });
-
-    $('#email').on('click', function(e){
-        window.location.href = 'mailto:jack.arendt1993@gmail.com';
-    });
 });
   }(jQuery));
