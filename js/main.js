a90b5fff02813ea3caae13f8d6f78666d5353ff0
jackarendt <jack.arendt93@gmail.com>
2015-11-01 16:45:33 -0800

    layout of home page done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 (function ($) {
   $(document).ready(function() {
-    var backgroundURL = '../img/ferrybuilding.jpg'
+    var backgroundURL = '../img/ferrybuilding.jpg';
+    if($(window).width() < 768) {
+        backgroundURL = '../img/ferrybuilding-small.jpg';
+    }
     var bgImage = new Image();
     bgImage.src = backgroundURL;
     bgImage.onload = function(){
@@ -9,12 +12,38 @@
         var scale = this.height/this.width;
         var width = $(window).width();
 
-        console.log(width * scale);
+        console.log("height: " + width * scale);
 
         main.css('background-image', 'url(' + backgroundURL + ')');
         main.css('width', '100%');
-        main.css('height', width * scale + 'px');
+        main.css('padding-top', $('nav').height() + 'px');
+        if (width > 768) {
+            main.css('height', width * scale + 'px');
+        } else {
+            main.css('height', width * scale * 2 + 'px');
+        }
+
     };
 
+    $('.navbar-brand').hide();
+    $(document.body).scroll(function (e) {
+        if (document.querySelectorAll('.about-page')[0].getBoundingClientRect().top - $('nav').height()< 0) {
+            $('.navbar-brand').fadeIn();
+        } else {
+            $('.navbar-brand').fadeOut();
+        }
+    });
+
+    $('#twitter').on('click', function(e){
+        window.location.href = 'http://twitter.com/woogidy_woogidy';
+    });
+
+    $('#linkedin').on('click', function(e){
+        window.location.href = 'http://linkedin.com/in/johnrarendt';
+    });
+
+    $('#email').on('click', function(e){
+        window.location.href = 'mailto:jack.arendt1993@gmail.com';
+    });
 });
   }(jQuery));
